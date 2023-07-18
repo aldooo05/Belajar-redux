f672be1cb58c9d2b7982f50254114561c48b7f1a
Mardotilah Darmawan <mardotilahdarmawan29@gmail.com>
2023-07-18 09:05:28 +0700

    hapus news membuat repo baru

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import LoginPage from "./component/login.component";
 import RegisterPage from "./component/register.component";
 import "bootstrap/dist/css/bootstrap.css";
-import NewsPage from "./component/news.component";
+
 
 function App() {
   const [isRegister, setIsRegister] = useState(false);
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <div>
         <div className="container" style={{ marginTop: "300px" }}>
-          {isRegister ? <LoginPage /> : <NewsPage />}
+          {isRegister ? <RegisterPage /> : <LoginPage/>}
           <span onClick={handleRegister}>Tidak punya akun? Buat disini</span>
         </div>
       </div>
